c079ded1c1d170b2b580abd89b4fd0ebf047444c
leighd2008 <leighd2008@gmail.com>
2020-02-10 15:23:04 -0500

    added basic components, created routes, connected firebase

---
+++ src/components/App/app.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navigation from "../Navigation/navigation";
+import LandingPage from "../Landing/landing";
+import SignUpPage from "../SignUp/signup";
+import SignInPage from "../SignIn/signin";
+import ForgotPasswordPage from "../ForgotPassword/forgotpassword";
+import HomePage from "../Home/home";
+import AccountPage from "../Account/account";
+import AdminPage from "../Admin/admin";
+
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
+
+      <hr />
+      <Route exact path="/landing" component={LandingPage} />
+      <Route path="/signup" component={SignUpPage} />
+      <Route path="/signin" component={SignInPage} />
+      <Route path="/pw-forgot" component={ForgotPasswordPage} />
+      <Route path="/home" component={HomePage} />
+      <Route path="/account" component={AccountPage} />
+      <Route path="/admin" component={AdminPage} />
+    </div>
+  </Router>
+);
+
+export default App;
